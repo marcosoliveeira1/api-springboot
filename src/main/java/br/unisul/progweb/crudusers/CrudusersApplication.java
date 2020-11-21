700bee5156694c45a019bc103c822353218023f0
Marcos Oliveira <marcos.oliveira@incuca.com.br>
2020-11-21 01:41:26 -0300

    create project

---
+++ src/main/java/br/unisul/progweb/crudusers/CrudusersApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.progweb.crudusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudusersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudusersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/unisul/progweb/crudusers/CrudusersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unisul.progweb.crudusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudusersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
