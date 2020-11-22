9886028d25e0acc5e78964bbfaecf663222e1a4d
Marcos Oliveira <marcos.oliveira@incuca.com.br>
2020-11-21 23:12:45 -0300

    update MaritalStatus to use lombok

--- src/main/java/br/unisul/progweb/crudusers/domain/MaritalStatus.java
+++ src/main/java/br/unisul/progweb/crudusers/domain/MaritalStatus.java
@@ -1,67 +1,26 @@
 package br.unisul.progweb.crudusers.domain;
 
+import com.sun.xml.internal.ws.developer.Serialization;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.io.Serializable;
-import java.util.Objects;
 
+@Data
 @Entity
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Serialization
 public class MaritalStatus implements Serializable {
-
 
     @Id
     @GeneratedValue
     private Integer id;
     private String name;
     private boolean isActive;
-
-    public MaritalStatus() {
-    }
-
-    public MaritalStatus(Integer id, String name, boolean isActive) {
-        this.id = id;
-        this.name = name;
-        this.isActive = isActive;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MaritalStatus that = (MaritalStatus) o;
-        return Objects.equals(id, that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
 
 }
